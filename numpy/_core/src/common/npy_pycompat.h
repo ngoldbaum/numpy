d3ada918bf7a2a5cb56226b57a0e2f5e50cade42
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-02-25 11:02:30 -0700

    ENH: add macro for locking a sequence while using PySequence_Fast

--- numpy/_core/src/common/npy_pycompat.h
+++ numpy/_core/src/common/npy_pycompat.h
@@ -6,4 +6,32 @@
 
 #define Npy_HashDouble _Py_HashDouble
 
+#if Py_GIL_DISABLED
+// adapted from CPython's "internal/pycore_critical_section.h"
+//
+// Specialized version of critical section locking to safely use
+// PySequence_Fast APIs without the GIL. For performance, the argument *to*
+// PySequence_Fast() is provided to the macro, not the *result* of
+// PySequence_Fast(), which would require an extra test to determine if the
+// lock must be acquired.
+#define NPY_BEGIN_CRITICAL_SECTION_SEQUENCE_FAST(original)         \
+    {                                                              \
+        PyObject *_orig_seq = (PyObject *)(original);              \
+        const int _should_lock_cs = PyList_CheckExact(_orig_seq);  \
+        PyCriticalSection _cs;                                     \
+        if (_should_lock_cs) {                                     \
+            PyCriticalSection_Begin(&_cs, _orig_seq);              \
+        }
+
+#define NPY_END_CRITICAL_SECTION_SEQUENCE_FAST()                   \
+        if (_should_lock_cs) {                                     \
+            PyCriticalSection_End(&_cs);                           \
+        }                                                          \
+    }
+
+#else
+#define NPy_BEGIN_CRITICAL_SECTION_SEQUENCE_FAST(original) {
+#define NPy_END_CRITICAL_SECTION_SEQUENCE_FAST() }
+#endif
+
 #endif  /* NUMPY_CORE_SRC_COMMON_NPY_PYCOMPAT_H_ */
